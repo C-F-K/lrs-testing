8a22cef5a0a81fbea506487df19ed78d498252d7
C-F-K <a.fell.ninny.mocker@gmail.com>
2015-11-26 14:44:07 +0100

    pages

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-app.use('/', express.static(__dirname + '/public'));
-app.use(express.static(__dirname));
-var port = process.env.PORT || 80;
-app.listen(port);
-console.log("Server is listening on port " + port);
